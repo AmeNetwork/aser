e9c5ceb57da978b059c9b1388d8b685524ba7dba
rickey <12867336+HelloRickey@users.noreply.github.com>
2025-03-19 15:26:19 +0800

    add cli

---
+++ esper/cli.py
@@ -0,0 +1,74 @@
+import cmd
+import sys
+import os
+import time
+from rich.console import Console
+from rich.spinner import Spinner
+from rich.text import Text
+from rich.panel import Panel
+from esper.agent import Agent
+from esper.memory import Memory
+import time
+
+
+class Cli(cmd.Cmd):
+
+    def __init__(self):
+        super().__init__()
+        self.console = Console()
+
+        self.uid = time.time()
+        self.memory = Memory(type="sqlite")
+        self.agent = Agent(
+            name="esper agent", model="gpt-3.5-turbo", memory=self.memory
+        )
+        intro_text = Text()
+
+        intro_text.append(
+            "How can I assist you today? (Type 'help' for commands)", style="green"
+        )
+
+        self.intro = intro_text
+        self.prompt = "Esper> "
+
+    def do_chat(self, arg):
+        with self.console.status("thinking", spinner="dots") as status:
+            result = self.agent.chat(arg, uid=self.uid)
+            result_text = Text()
+            result_text.append(result, style="green")
+            self.console.print(result_text)
+
+    def do_help(self, arg):
+
+        help_text = Text()
+        help_text.append("\n", style="cyan")
+        help_text.append("  enter text directly to start chatting \n", style="cyan")
+        help_text.append("  help  - show this help information\n", style="cyan")
+        help_text.append("  clear - clear chat history\n", style="cyan")
+        help_text.append("  exit  - exit program\n", style="cyan")
+        self.console.print(
+            Panel(help_text, title="Help", expand=False, border_style="cyan")
+        )
+
+    def do_clear(self, arg):
+        self.memory.clear(self.uid)
+        os.system("cls" if os.name == "nt" else "clear")
+
+    def do_exit(self, arg):
+
+        return True
+
+    def default(self, arg):
+        self.do_chat(arg)
+
+    def cmdloop(self, intro=None):
+        self.console.print(self.intro)
+        try:
+            super(Cli, self).cmdloop(intro="")
+        except KeyboardInterrupt:
+            print("\nBye!")
+            return
+
+
+if __name__ == "__main__":
+    Cli().cmdloop()
